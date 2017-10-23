bc0b9afefe1d1c63d209dd4f2839c9db5819abe9
Hoang Nguyen <nhathoang989@gmail.com>
2017-10-23 20:23:30 +0700

    aaaa

--- src/app/@theme/components/footer/footer.component.ts
+++ src/app/@theme/components/footer/footer.component.ts
@@ -4,7 +4,8 @@ import { Component } from '@angular/core';
   selector: 'ngx-footer',
   styleUrls: ['./footer.component.scss'],
   template: `
-    <span class="created-by">Created with ♥ by <b><a href="http://swastika.io" target="_blank">Swastika I/O</a></b> 2017</span>
+    <span class="created-by">Created with ♥ by <b>
+    <a href="http://swastika.io" target="_blank">Swastika I/O</a></b> 2017</span>
     <div class="socials">
       <a href="#" target="_blank" class="ion ion-social-github"></a>
       <a href="#" target="_blank" class="ion ion-social-facebook"></a>
